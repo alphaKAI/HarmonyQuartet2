f7980fd466cf56592ce9b2fa06009d69475e075f
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-01-24 17:10:08 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 npm install
+cd semantic
+gulp build
+cd ../
 tsc app.ts --module commonjs
 cd ts/gulp
 npm install
@@ -7,4 +10,12 @@ gulp browserify
 gulp minify
 cd ../
 
+cat << EOT > setting.json
+{
+  "consumer_key"        : "Your Consumer Key", 
+  "consumer_secret"     : "Your Consumer Secret"
+}
+EOT
+
 echo "[HarmonyQuartet2 - Installation] Finish"
+echo 'You should configure "setting.json"'
